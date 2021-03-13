8bd22e9ec673f3ce139db394a2865b4dcd822718
christopher-christofi <christopherlchristofi@outlook.com>
2021-03-13 15:33:42 +0000

    Initial update, function sql query all users

--- data/data.js
+++ data/data.js
@@ -4,7 +4,7 @@
 const sqlite3 = require("sqlite3").verbose();
 
 // The application layer uses student classes
-const student = require("../library.js");
+const user = require("../library.js");
 
 // Initiate database connection
 var db = new sqlite3.Database("data/database.db", function(err) {
@@ -13,3 +13,39 @@ var db = new sqlite3.Database("data/database.db", function(err) {
     }
     console.log("Connected to library database.");
 });
+
+// Export getUsers function
+exports.getUsers = function(callback) {
+    // Create SQL statement
+    var sql = `
+        SELECT
+            users.id,
+            users.email,
+            credentials.password
+        FROM
+            users,
+            credentials
+        WHERE
+            users.id = credentials.user_id
+        `;
+    // Execute query. Return all
+    db.all(sql, function(err, rows) {
+        // Check if error
+        if (err) {
+            return console.error(err.message);
+        }
+        // Create an array of users
+        var user_collection = [];
+        // Create loop through the rows to build user objects
+        for (var row of rows) {
+            // Create credentials object
+            var credential = new user.User(row.password);
+            // Create user object
+            var user_detail = new user.User(row.id, row.email, credential);
+            // Add user to created array
+            user_collection.push(user_detail);
+        }
+        // Execute callback function
+        callback(user_collection);
+    });
+};

7d3157b2ddc4fce1874cccd01409a0b49f63df3a
christopher-christofi <christopherlchristofi@outlook.com>
2021-03-13 00:20:49 +0000

    Initial commit set up endpoint application

---
+++ app/app.js
@@ -0,0 +1,60 @@
+"use strict";
+
+// The application layer to use user and item classes
+const library = require("../library.js");
+
+// The application layer to access the data layer
+const data = require("../data/data.js");
+
+// Import express library
+const express = require("express");
+
+// Value express application
+var app = express();
+
+// Add static files location
+app.use(express.static("static"));
+
+// Add /user endpoint
+app.get("/user/:email", function(req, res) {
+    // Return user detail
+    res.send("User email: " + req.params.email);
+});
+
+// Add /users endpoint
+app.get("/users", function(req, res) {
+    // Return all users
+    res.send("All users");
+});
+
+// Add /book endpoint
+app.get("/book/:title", function(res, req) {
+    // Return book detail
+    res.send("Book title: " + req.params.title);
+});
+
+// Add /books endpoint
+app.get("/books", function(req, res) {
+    // Return all info links
+    res.send("All books");
+});
+
+// Add /category endpoint
+app.get("/category/:category", function(res, req) {
+    // Return categroup group option
+    res.send("Category: " + req.params.category);
+});
+
+// Add /categories endpoint
+app.get("/categories", function(res, req) {
+    // Return all category options
+    res.send("All categories");
+});
+
+// Initiate listen on port 3000
+app.listen(3000, function(err) {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Server started.");
+});

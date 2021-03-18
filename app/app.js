b3d8b8bfebb2870005985f5329f804cffd8b1911
christopher-christofi <christopherlchristofi@outlook.com>
2021-03-18 20:59:32 +0000

    Updated endpoints, spacing

--- app/app.js
+++ app/app.js
@@ -17,56 +17,56 @@ app.use(express.static("static"));
 
 // Add /user endpoint
 app.get("/user/:email", (req, res) => {
-        // Call getUserProfile from data
-        data.getUserProfile(req.params.email, (user_profile) => {
-                res.json(user_profile);
-            });
+    // Call getUserProfile from data
+    data.getUserProfile(req.params.email, (user_profile) => {
+        res.json(user_profile);
     });
+});
 
 // Add /users endpoint
 app.get("/users", (req, res) => {
-        // Call getUsers from data
-        data.getUsers((user_collection) => {
-                res.json(user_collection);
-            });
+    // Call getUsers from data
+    data.getUsers((user_collection) => {
+        res.json(user_collection);
     });
+});
 
 // Add /book endpoint
 app.get("/book/:title", (req, res) => {
-        // Call getBook from data
-        data.getBook(req.params.title, (book_item) => {
-                res.json(book_item);
-            });
+    // Call getBook from data
+    data.getBook(req.params.title, (book_item) => {
+        res.json(book_item);
     });
+});
 
 // Add /books endpoint
-app.get("/books", (req, res) => {
-        // Call getBookItems from data
-        data.getBookItems((book_collection) => {
-                res.json(book_collection);
-            });
+app.get("/all-books", (req, res) => {
+    // Call getBookItems from data
+    data.getBookItems((book_collection) => {
+        res.json(book_collection);
     });
+});
 
 // Add /genre endpoint
 app.get("/genre/:genre", (req, res) => {
-        // Call getGenre callback function from data
-        data.getGenre(req.params.genre, (genre_selected) => {
-                res.json(genre_selected);
-            });
+    // Call getGenre callback function from data
+    data.getGenre(req.params.genre, (genre_selected) => {
+        res.json(genre_selected);
     });
+});
 
 // Add /all-genre endpoint
 app.get("/all-genre", (req, res) => {
-        // Call getAllGenre from data
-        data.getAllGenre((genre_types) => {
-                res.json(genre_types);
-            });
+    // Call getAllGenre from data
+    data.getAllGenre((genre_types) => {
+        res.json(genre_types);
     });
+});
 
 // Initiate listen on port 3000
 app.listen(3000, (err) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log("Server listening on port 3000.");
-    });
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Server listening on port 3000.");
+});

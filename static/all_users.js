ecdcba905dd5e4351f0bce327a4fa48c87ab4dec
christopher-christofi <christopherlchristofi@outlook.com>
2021-03-20 01:21:57 +0000

    Initial updated controller user select func added

--- static/all_users.js
+++ static/all_users.js
@@ -9,4 +9,11 @@ mainApp.controller("usersController", ($scope, $http) => {
     $http.get('/all-users').then((response) => {
         $scope.users = response.data;
     });
+
+    $scope.selectUser = (id) => {
+        // Retrieve user
+        $http.get("/user/" + id).then((response) => {
+            $scope.selectedUser = response.data;
+        });
+    }
 });

f917cdcfe5d92ef97ab910913c7804419e1fafd2
christopher-christofi <christopherlchristofi@outlook.com>
2021-03-29 18:27:19 +0100

    Updated user table format design align overflow

--- data/database.sql
+++ data/database.sql
@@ -10,7 +10,7 @@ INSERT INTO users VALUES('test2@email.com',3);
 INSERT INTO users VALUES('test3@email.com',4);
 INSERT INTO users VALUES('updatedemail1@email.com',1616446450377);
 INSERT INTO users VALUES('createduser1@email.com',1616619483785);
-INSERT INTO users VALUES('testeremail@email.co.uk',1616879856527);
+INSERT INTO users VALUES('test@email.com',1617038688381);
 CREATE TABLE credentials (
     password TEXT,
     user_id INTEGER (255),
@@ -22,7 +22,7 @@ INSERT INTO credentials VALUES('BBB222',3);
 INSERT INTO credentials VALUES('CCC333',4);
 INSERT INTO credentials VALUES('password1',1616446450377);
 INSERT INTO credentials VALUES('Pass123',1616619483785);
-INSERT INTO credentials VALUES('fvygvbjnjkmkl,',1616879856527);
+INSERT INTO credentials VALUES('1234321',1617038688381);
 CREATE TABLE admin_access (
     access INTEGER (1),
     user_id INTEGER (255),
@@ -34,7 +34,7 @@ INSERT INTO admin_access VALUES(0,3);
 INSERT INTO admin_access VALUES(0,4);
 INSERT INTO admin_access VALUES(0,1616446450377);
 INSERT INTO admin_access VALUES(0,1616619483785);
-INSERT INTO admin_access VALUES(0,1616879856527);
+INSERT INTO admin_access VALUES(0,1617038688381);
 CREATE TABLE books (
     title VARCHAR (50) PRIMARY KEY,
     author VARCHAR (50),

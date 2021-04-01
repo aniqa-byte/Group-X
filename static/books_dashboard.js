68ff3e0175e9c3950a043858b5a1661996977349
christopher-christofi <christopherlchristofi@outlook.com>
2021-04-01 23:22:33 +0100

    Updated version of genre selection method

--- static/books_dashboard.js
+++ static/books_dashboard.js
@@ -9,13 +9,24 @@ mainApp.controller("booksController", ($scope, $http) => {
     // Hide the "selected" element of the bookcontroller
     document.getElementById("selected").style.display="none";
 
-    $http.get('/all-books').then((response) => {
+    $http.get("/all-books").then((response) => {
         $scope.books = response.data;
     });
 
-    $http.get('/all-book-genres').then((response) => {
+    $http.get("/all-book-genres").then((response) => {
         $scope.book_genres = response.data;
     });
+
+    $scope.updateGenreSelection = () => {
+
+        $scope.selectedGenre = new Book_genre (genre);
+
+        $http.get("/books-genre-match/" + $scope.selectedGenre.genre).then((response) => {
+            $scope.selectedGenre = new Book_genre ("");
+            $scope.books = response.data;
+            document.getElementById("books_main").style.display="block";
+        });
+    };
 
     $scope.selectBook = (title) => {
     // Locate matching details

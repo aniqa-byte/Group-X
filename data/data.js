3c8a56d58e4c5640440c5d283dfaff789783537b
christopher-christofi <christopherlchristofi@outlook.com>
2021-04-02 01:22:53 +0100

    Updated database, auto build method serialized fun

--- data/data.js
+++ data/data.js
@@ -7,13 +7,49 @@ const sqlite3 = require("sqlite3").verbose();
 const library = require("../library.js");
 
 // Initiate database connection
-var db = new sqlite3.Database("data/database.db", (err) => {
+// var db = new sqlite3.Database("data/database.db", (err) => { // Prior development method -> utilises database.sql
+var db = new sqlite3.Database(":memory:", (err) => {
         if (err) {
             return console.error(err.message);
         }
         console.log("Connected to library database.");
     });
 
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS users (email TEXT PRIMARY KEY, id INTEGER (255))");
+    db.run("CREATE TABLE IF NOT EXISTS credentials (password TEXT, user_id INTEGER (255), FOREIGN KEY (user_id) REFERENCES users (id))");
+    db.run("CREATE TABLE IF NOT EXISTS admin_access (access INTEGER (1), user_id INTEGER (255), FOREIGN KEY (user_id) REFERENCES users (id))");
+    db.run("CREATE TABLE IF NOT EXISTS books (title VARCHAR (50) PRIMARY KEY, author VARCHAR (50), genre VARCHAR (50), FOREIGN KEY (genre) REFERENCES book_genre (genre))");
+    db.run("CREATE TABLE IF NOT EXISTS book_details (title VARCHAR (50), item_link TEXT, item_description TEXT, FOREIGN KEY (title) REFERENCES books (title))");
+    db.run("CREATE TABLE IF NOT EXISTS book_genre (genre VARCHAR (50) PRIMARY KEY, genre_description TEXT)");
+});
+
+db.serialize(() => {
+    db.run("INSERT INTO users VALUES('ROOT',1)");
+    db.run("INSERT INTO credentials VALUES('ROOT',1)");
+    db.run("INSERT INTO admin_access VALUES(1,1)");
+    db.run("INSERT INTO users VALUES('test1@email.com',2)");
+    db.run("INSERT INTO credentials VALUES('AAA111',2)");
+    db.run("INSERT INTO admin_access VALUES(1,2)");
+    db.run("INSERT INTO books VALUES('Title 1','Author 1','Genre 1')");
+    db.run("INSERT INTO books VALUES('Title 2','Author 1','Genre 1')");
+    db.run("INSERT INTO books VALUES('Title 3','Author 2','Genre 1')");
+    db.run("INSERT INTO books VALUES('Title 4','Author 3','Genre 2')");
+    db.run("INSERT INTO books VALUES('Title 5','Author 4','Genre 3')");
+    db.run("INSERT INTO books VALUES('Title 6','Author 4','Genre 3')");
+    db.run("INSERT INTO book_details VALUES('Title 1','https://www.google.co.uk','1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
+    db.run("INSERT INTO book_details VALUES('Title 2','https://www.google.co.uk','2Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
+    db.run("INSERT INTO book_details VALUES('Title 3','https://www.google.co.uk','3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
+    db.run("INSERT INTO book_details VALUES('Title 4','https://www.google.co.uk','4Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
+    db.run("INSERT INTO book_details VALUES('Title 5','https://www.google.co.uk','5Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
+    db.run("INSERT INTO book_details VALUES('Title 6','https://www.google.co.uk','6Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
+    db.run("INSERT INTO book_genre VALUES('Genre 1','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
+    db.run("INSERT INTO book_genre VALUES('Genre 2','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
+    db.run("INSERT INTO book_genre VALUES('Genre 3','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
+    db.run("INSERT INTO book_genre VALUES('Genre 4','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
+});
+
+// TODO: unused in login feature (query is functional)
 exports.validateAdmin = (email, password, callback) => {
     // Create SQL statement
     var sql = `
@@ -105,14 +141,18 @@ exports.getUser = (email, callback) => {
         `;
     // Execute query. Returning user row matching email.
     db.get(sql, (err, row) => {
-            if (err) {
-                return console.error(err.message);
-            }
+
+        try {
             // Create a user object
             var user = new library.User(row.id, row.email);
             // Return profile
             callback(user);
-        });
+        }
+        catch(err) {
+            return console.error(err.message);
+
+        }
+    });
 };
 
 exports.registerUser = (user, callback) => {

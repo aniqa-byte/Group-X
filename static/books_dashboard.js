6a59c09155b2b96d297b8a22f8b003d293783810
christopher-christofi <christopherlchristofi@outlook.com>
2021-04-04 20:22:46 +0100

    Added book genre edit methods and functionality

--- static/books_dashboard.js
+++ static/books_dashboard.js
@@ -8,6 +8,7 @@ mainApp.controller("booksController", ($scope, $http) => {
 
     // Hide the "selected" element of the bookcontroller
     document.getElementById("selected").style.display="none";
+    document.getElementById("edit_selected").style.display="none";
     document.getElementById("search_results").style.display="none";
 
     $http.get("/all-books").then((response) => {
@@ -43,6 +44,37 @@ mainApp.controller("booksController", ($scope, $http) => {
         $scope.selectedBook = response.data;
         // Display the "selected" element once item has been selected
         document.getElementById("selected").style.display="block";
+        document.getElementById("edit_selected").style.display="none";
         });
+    };
+
+    $scope.editBook = (title) => {
+
+        $http.get("/search-book/" + title).then((response) => {
+            $scope.editSelected = response.data;
+            $scope.selectedBook = response.data;
+            document.getElementById("edit_selected").style.display="block";
+        })
+    };
+
+    $scope.updateGenre = (title, genre) => {
+        $scope.update_genre = new Book_complete (title, "", genre, "", "");
+        $http.put("/update-book-genre/", $scope.update_genre).then(() => {
+            console.log("Genre Updated: " + $scope.update_genre.genre)
+            $http.get("/all-books").then((response) => {
+                $scope.update_genre = new Book_complete ("", "", "", "", "");
+                $scope.books = response.data;
+            });
+        })
+    }
+
+    $scope.updateLink = (title, item_link) => {
+        $scope.update_genre = new Book_complete (title, "", "", item_link, "");
+        $http.put("/update-book-link/", $scope.update_link).then(() => {
+            console.log("Link Updated: " + $scope.update_link.item_link)
+            $http.get("/all-books").then((response) => {
+                $scope.books = response.data;
+            });
+        })
     }
  });
